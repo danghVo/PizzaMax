546a1dd89d555bffd588e63a1f02f65775115df3
Vo <vob2014721@student.ctu.edu.vn>
2023-09-19 19:44:30 +0700

    Modify data add to selection table when add to Cart and Add func getCartOfUser

--- pizzamax-backend/src/controller/CartController.js
+++ pizzamax-backend/src/controller/CartController.js
@@ -6,6 +6,18 @@ class CartController extends Controller {
     constructor() {
         super('CartController');
     }
+
+    async addToCart(req, res) {
+        try {
+            const cartUUID = req.params.uuid;
+
+            await CartService.addToCart(cartUUID, req.body);
+
+            return res.send('Add product successfully');
+        } catch (error) {
+            return res.send(error?.message || error);
+        }
+    }
 }
 
 module.exports = new CartController();
