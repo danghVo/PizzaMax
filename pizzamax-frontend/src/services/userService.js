4dcc9facb534e7482a258787a7b2070738f399ec
Vo <vob2014721@student.ctu.edu.vn>
2023-09-27 16:28:24 +0700

    Create get user infor by token

--- pizzamax-frontend/src/services/userService.js
+++ pizzamax-frontend/src/services/userService.js
@@ -4,8 +4,12 @@ const path = 'user/';
 
 export const register = async (payload, option = {}) => {
     const res = await httpRequest.post(path + 'register', payload, option);
-    const status = res.split(' ')[0];
-    const message = res.replace(status, '').trim();
 
-    return parseInt(status) ? message : '';
+    return res;
+};
+
+export const getInforByToken = async (option = {}) => {
+    const res = await httpRequest.get(path + 'getInforByToken');
+
+    return res;
 };
