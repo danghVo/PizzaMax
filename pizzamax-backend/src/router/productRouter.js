8b27b8304007557efbc89a57e1cc5f610d871c65
Vo <vob2014721@student.ctu.edu.vn>
2023-09-27 16:20:55 +0700

    Add libs

--- pizzamax-backend/src/router/productRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { ProductController } = require('../controller');
-
-router.get('/product/getAll', ProductController.getAll);
-
-module.exports.name = 'product';
-module.exports.router = router;
