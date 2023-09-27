55db27057589e888e4825a8a33f94171a6b529e8
Vo <vob2014721@student.ctu.edu.vn>
2023-09-27 15:57:26 +0700

    Create checkout controller

--- pizzamax-backend/src/controller/CartController.js
+++ pizzamax-backend/src/controller/CartController.js
@@ -7,15 +7,60 @@ class CartController extends Controller {
         super('CartController');
     }
 
-    async addToCart(req, res) {
+    async checkCart(req, res, next) {
         try {
             const cartUUID = req.params.uuid;
 
-            await CartService.addToCart(cartUUID, req.body);
+            const cart = await CartService.getCart(cartUUID);
+
+            res.locals.cart = cart;
+            next();
+        } catch (error) {
+            return res.status(error.status || 500).send(error.message || error);
+        }
+    }
+
+    async addToCart(req, res) {
+        try {
+            const cart = res.locals.cart;
+
+            await CartService.addToCart(cart, req.body);
 
             return res.send('Add product successfully');
         } catch (error) {
-            return res.send(error?.message || error);
+            return res.status(error.status || 500).send(error.message || error);
+        }
+    }
+
+    async removeFromCart(req, res) {
+        try {
+            await CartService.removeFromCart(req.body);
+
+            return res.send('Remove product successfully');
+        } catch (error) {
+            return res.status(error.status || 500).send(error.message || error);
+        }
+    }
+
+    async updateProduct(req, res) {
+        try {
+            await CartService.updateProduct(req.body);
+
+            return res.send('Update product successfully');
+        } catch (error) {
+            return res.status(error.status || 500).send(error.message || error);
+        }
+    }
+
+    async checkout(req, res) {
+        try {
+            const cart = res.locals.cart;
+
+            const message = await CartService.checkout(cart, req.body);
+
+            return res.send(message);
+        } catch (error) {
+            return res.status(error.status || 500).send(error.message || error);
         }
     }
 }
