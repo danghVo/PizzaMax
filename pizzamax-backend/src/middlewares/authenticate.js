c4ea7d2d649c889e69997a976cd8f0ca8c3d7110
Vo <vob2014721@student.ctu.edu.vn>
2023-09-27 16:00:16 +0700

    Move authenticate from valid to middlewares

---
+++ pizzamax-backend/src/middlewares/authenticate.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+const path = require('path');
+const throwError = require('../utils/throwError');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+
+const exceptions = ['/api/auth/', '/api/user/register', '/api/product/getAll', '/api/address/getAllShop'];
+
+module.exports = function authenticate(req, res, next) {
+    try {
+        const path = req.url;
+        let isException = false;
+
+        exceptions.forEach((exception) => {
+            if (path.includes(exception)) {
+                isException = true;
+                return next();
+            }
+        });
+
+        if (isException) return;
+
+        const { accessToken } = req.cookies['token'];
+
+        const decoded = jwt.verify(
+            accessToken,
+            path.includes('/admin/') ? process.env.ADMIN_TOKEN_SECRET : process.env.ACCESS_TOKEN_SECRET,
+        );
+
+        if (decoded) {
+            res.locals.phoneNumber = decoded.phoneNumber;
+            res.locals.password = decoded.password;
+            return next();
+        }
+
+        return throwError(403, 'Dont have right to access');
+    } catch (error) {
+        return res.status(error.status || 500).send(error.message || error);
+    }
+};
