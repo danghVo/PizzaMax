9cdb3951e0a0a399a53208986c3cc26d9fe0e3dc
Vo <vob2014721@student.ctu.edu.vn>
2023-09-27 16:36:57 +0700

    Add interceptors

--- pizzamax-frontend/src/utils/httpRequest.js
+++ pizzamax-frontend/src/utils/httpRequest.js
@@ -2,7 +2,27 @@ import axios from 'axios';
 
 const httpRequest = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
+    withCredentials: true,
 });
+
+// httpRequest.interceptors.request.use((config) => {
+//     // if(document.cookie)
+//     return config;
+// });
+
+httpRequest.interceptors.response.use(
+    (respone) => respone,
+    async (error) => {
+        if (error.response.data === 'jwt expired') {
+            await httpRequest.get('/auth/refreshToken');
+            const prevRequest = error?.config;
+            prevRequest.sent = true;
+            return await httpRequest(prevRequest);
+        }
+
+        return Promise.reject(error);
+    },
+);
 
 export const get = async (path, option = {}) => {
     const respone = await httpRequest.get(path, option);
