cf317f7cb42d1600c469442ad177aa588785b122
Vo <vob2014721@student.ctu.edu.vn>
2023-09-27 16:27:59 +0700

    Create data transform func

--- pizzamax-frontend/src/services/productService.js
+++ pizzamax-frontend/src/services/productService.js
@@ -1,9 +1,19 @@
-import * as httpRequest from '~/utils/httpRequest'
+import * as httpRequest from '~/utils/httpRequest';
 
-const path = 'product/'
+const path = 'product/';
+
+const dataTransform = (data) => {
+    return data.map((item) => ({
+        ...item,
+        discOptions: item.discOptions.map((disc) => ({
+            ...disc,
+            subOptions: disc.subOptions.sort((a, b) => a.price - b.price),
+        })),
+    }));
+};
 
 export const getAll = async () => {
     const res = await httpRequest.get(path + 'getAll');
 
-    return res;
+    return dataTransform(res);
 };
