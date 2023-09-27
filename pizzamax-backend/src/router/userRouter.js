ba2cc4088693bfd7c6936c0aaef0138e2b8055ca
Vo <vob2014721@student.ctu.edu.vn>
2023-09-27 16:05:47 +0700

    Create add favor adn remove favor route

--- pizzamax-backend/src/router/userRouter.js
+++ pizzamax-backend/src/router/userRouter.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
-const { UserController } = require('../controller');
+const { UserController, AddressController, ProductController } = require('../controller');
 
 router.post('/user/register', UserController.register);
-router.patch('/user/changePassword/:uuid', UserController.changePassword);
-router.patch('/user/update/:uuid', UserController.updateUser);
+router.get('/user/getInforByToken', UserController.getInforByToken);
+
+router.all('/user/:uuid/*', UserController.checkUser);
+
+router.patch('/user/:uuid/changePassword', UserController.changePassword);
+router.patch('/user/:uuid/update', UserController.updateUser);
+
+router.post('/user/:uuid/newAddress', AddressController.newAddress);
+router.patch('/user/:uuid/editAddress/:addressId', AddressController.editAddress);
+router.delete('/user/:uuid/deleteAddress/:addressId', AddressController.deleteAddress);
+
+router.post('/user/:uuid/addFavor/:productId', ProductController.addFavor);
+router.delete('/user/:uuid/removeFavor/:productId', ProductController.removeFavor);
 
 module.exports.name = 'user';
 module.exports.router = router;
