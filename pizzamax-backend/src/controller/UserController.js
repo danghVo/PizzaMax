65eba6d044db126a9dd6b5548795b307f37310c2
Vo <vob2014721@student.ctu.edu.vn>
2023-09-27 15:59:30 +0700

    Create get infor by token

--- pizzamax-backend/src/controller/UserController.js
+++ pizzamax-backend/src/controller/UserController.js
@@ -1,5 +1,5 @@
 const throwError = require('../utils/throwError');
-const { UserService } = require('../service');
+const { UserService, AuthService } = require('../service');
 const Controller = require('./Controller');
 
 class UserController extends Controller {
@@ -7,17 +7,43 @@ class UserController extends Controller {
         super('UserController');
     }
 
-    async login(req, res, next) {
+    async checkUser(req, res, next) {
         try {
-            const userValid = await UserService.validUser(req.body);
-
-            if (userValid) {
-                next();
-            } else {
-                throwError(401, 'wrong password');
-            }
+            const uuid = req.params.uuid;
+            const userExist = await UserService.userExist(uuid);
+            res.locals.user = userExist;
+            next();
         } catch (error) {
-            return res.send(error?.message || error);
+            return res.status(error.status || 500).send(error.message || error);
+        }
+    }
+
+    async getUserInfor(req, res, next) {
+        try {
+            const user = res.locals.user;
+            if (user.getDataValue('role') !== 'admin') {
+                const userInfor = await UserService.getUserInfor(user);
+                res.locals.user = userInfor;
+            } else res.locals.user = user.toJSON();
+
+            next();
+        } catch (error) {
+            return res.status(error.status || 500).send(error.message || error);
+        }
+    }
+
+    async getInforByToken(req, res) {
+        try {
+            const user = await AuthService.validUser({
+                phoneNumber: res.locals.phoneNumber,
+                password: res.locals.password,
+            });
+
+            const userInfor = await UserService.getUserInfor(user);
+
+            return res.json(userInfor);
+        } catch (error) {
+            return res.status(error.status || 500).send(error.message || error);
         }
     }
 
@@ -27,37 +53,37 @@ class UserController extends Controller {
 
             return res.status(200).send('User was created successfully');
         } catch (error) {
-            return res.send(error?.message || error);
+            return res.status(error.status || 500).send(error.message || error);
         }
     }
 
     async updateUser(req, res, next) {
         try {
             const payload = req.body;
-            const uuid = req.params.uuid;
+            const user = res.locals.user;
 
             if (payload) {
                 delete payload.phoneNumber;
                 delete payload.password;
             }
 
-            await UserService.updateUser(uuid, payload);
+            await UserService.updateUser(user, payload);
 
             res.status(200).send('Update successfully');
         } catch (error) {
-            return res.send(error?.message || error);
+            return res.status(error.status || 500).send(error.message || error);
         }
     }
 
     async changePassword(req, res) {
         try {
-            const uuid = req.params.uuid;
+            const user = res.locals.user;
 
-            await UserService.changePassword(uuid, req.body);
+            await UserService.changePassword(user, req.body);
 
             res.status(200).send('Change password successfully');
         } catch (error) {
-            return res.send(error?.message || error);
+            return res.status(error.status || 500).send(error.message || error);
         }
     }
 }
