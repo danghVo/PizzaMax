11ae088364ab4f27ad79febf2484ada8ce2b205b
Vo <vob2014721@student.ctu.edu.vn>
2023-09-27 16:04:35 +0700

    Create route checkout

--- pizzamax-backend/src/router/cartRouter.js
+++ pizzamax-backend/src/router/cartRouter.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const { CartController } = require('../controller');
 
+router.all('/cart/:uuid/*', CartController.checkCart);
+
 router.post('/cart/:uuid/addToCart', CartController.addToCart);
-router.post('/cart/:uuid/removeFromCart', CartController.removeFromCart);
+router.delete('/cart/:uuid/removeFromCart', CartController.removeFromCart);
 router.patch('/cart/:uuid/updateProduct', CartController.updateProduct);
-router.post('/cart/:uuid/checkout', CartController.checkout);
+router.patch('/cart/:uuid/checkout', CartController.checkout);
 
 module.exports.name = 'cart';
 module.exports.router = router;
