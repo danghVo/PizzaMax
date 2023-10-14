2f875fcd07d453b48927b2817224651b13741c32
Vo <vob2014721@student.ctu.edu.vn>
2023-10-14 12:18:18 +0700

    Add dataTransform to response

--- pizzamax-frontend/src/services/cartService.js
+++ pizzamax-frontend/src/services/cartService.js
@@ -1,17 +1,17 @@
-import { httpRequest } from '~/utils';
+import { httpRequest, dataTransform } from '~/utils';
 
 const path = 'cart/';
 
 export const addToCart = async (payload, option = {}) => {
     const res = await httpRequest.post(path + payload.uuid + '/addToCart', payload);
 
-    return res;
+    return dataTransform.cart(res);
 };
 
 export const removeFromCart = async (payload, option = {}) => {
     const res = await httpRequest.destroy(path + payload.uuid + '/removeFromCart/' + payload.detailUUID);
 
-    return res;
+    return dataTransform.cart(res);
 };
 
 export const increase = async (payload, option = {}) => {
@@ -19,7 +19,7 @@ export const increase = async (payload, option = {}) => {
         quantity: payload.quantity,
     });
 
-    return res;
+    return dataTransform.cart(res);
 };
 
 export const decrease = async (payload, option = {}) => {
@@ -27,5 +27,5 @@ export const decrease = async (payload, option = {}) => {
         quantity: payload.quantity,
     });
 
-    return res;
+    return dataTransform.cart(res);
 };
